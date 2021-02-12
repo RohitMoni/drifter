2c811b887b5e52ce73d1a83d80f1d7d9faa04e23
Rohit Moni <rohitmdxb@gmail.com>
2021-02-12 00:31:30 -0500

    Added basic samples

---
+++ Drifter.Samples/DatabaseMigration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatabaseMigration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
